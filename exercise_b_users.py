0150c610cc3ed285a0195acb73df3a850dab966c
Nathan <nathan.rendall@gmail.com>
2021-06-15 22:25:11 +0100

    Corrected mistake, name changed to species for Q4

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -61,7 +61,7 @@ print(users["Erik"]["home_town"])
 # 3. Get the array of Erik's lottery numbers
 print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
-print(users["Avril"]["pets"][0]["name"])  
+print(users["Avril"]["pets"][0]["species"])  
 # 5. Get the smallest of Erik's lottery numbers
 smallest_number = 100
 for number in users["Erik"]["lottery_numbers"]:
